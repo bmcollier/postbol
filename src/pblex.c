bf8aa131487f4104192adbf57c77db5b67cfb007
Ben Collier <bmcollier@gmail.com>
2020-04-21 10:04:17 +0100

    Added args to pass filename to lexer, and line-by-line process.

--- src/pblex.c
+++ src/pblex.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char  *argv [ ] )
 {
     printf(" \n PostBOL Lexer: %s \t", argv[0]);
 
-    if( argc == 2 )
-    {
-        printf("\n Value given by user is: %s \t", argv[1]);
+    printf("Lexing file %s", argv[1]);
+    FILE *fp;
+    char buff[1024];
+
+    fp = fopen(argv[1], "r");
+
+    if (!fp) {      /* validate file is open */
+        fprintf (stderr, "processText() error: file open failed '%s'.\n", "test");
+        exit(EXIT_FAILURE);
     }
-    else if( argc > 2 )
-    {
-        printf("\n Many values given by users.\n");
+
+    printf("\n");
+
+    while( fgets(buff ,1024,fp) ) {
+        printf("%s", buff);
     }
-    else
-    {
-        printf(" \n Single value expected.\n");
-    }
+
+    return 0;
+
 }
+
